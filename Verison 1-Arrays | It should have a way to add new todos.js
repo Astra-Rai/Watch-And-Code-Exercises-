a1571d2b8b5ca1f2030425b9941494601c1f9304
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-16 19:04:50 -0600

    V1- Arrays: It should have a way to add new todos
    //Section Objective:  It should have a way to add new todos
    
    //Notes:
    
    //Currently, we're working from the console
    //We want to add an element to our array
    //The push() method adds one or more elements to the end of an array and returns the new length of the array.
    //push() syntax: arr.push(element1[, ...[, elementN]])

---
+++ Verison 1-Arrays | It should have a way to add new todos.js
@@ -0,0 +1,24 @@
+
+//Watch and Code Course
+//Instructor: Gordon Zhu
+//Student: Astra Rai
+//Version 1 | Array
+//Section Objective: It should have a way to add new todos display 
+
+//Notes:
+
+//Currently, we're working from the console
+//We want to add an element to our array
+//The push() method adds one or more elements to the end of an array and returns the new length of the array.
+//push() syntax: arr.push(element1[, ...[, elementN]])
+
+//variable name "todas", an array with three elements
+var todos = ['item 1', 'item 2', 'item 3'];
+
+//use push() to add 'item 4' to "todos" array
+todos.push('item 4');
+
+//print todos array in console
+
+console.log(todos);
+//expected output: ["item 1", "item 2", "item 3", "item 4"]
