f1e62e1cc3a3666d4abbcd61d85466a461cb92cf
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-17 14:49:23 -0600

    V2-Functions: Function to display todos
    //Watch and Code Course
    //Instructor: Gordon Zhu
    //Student: Astra Rai
    //Version 2 | Functions
    //Section Objective: It should have a function to display todos

---
+++ Version-2-Functions | It should have a function to display todos.js
@@ -0,0 +1,16 @@
+//Watch and Code Course
+//Instructor: Gordon Zhu
+//Student: Astra Rai
+//Version 2 | Functions
+//Section Objective: It should have a function to display todos
+
+//create an array to store todo elements
+var todos = ['item 1', 'item 2', 'item 3'];
+//create a function called "displayTodos"
+function displayTodos(){
+  //print the text My Todos: and elements in the array named "todos"
+  console.log('My Todos: ', todos);
+}
+//below is how to call a fuction, it will run code in-between functions {} braces
+displayTodos();
+//expectd output: "My Todos: " ["item 1", "item 2", "item 3"]
