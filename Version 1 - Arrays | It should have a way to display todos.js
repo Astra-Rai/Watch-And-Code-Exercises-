3185922908598033e35e7816abb89235e2761411
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-16 18:25:44 -0600

    V1-Arrays | It should have a way to display todos

---
+++ Version 1 - Arrays | It should have a way to display todos.js
@@ -0,0 +1,17 @@
+
+//Watch and Code Course
+//Instructor: Gordon Zhu
+//Student: Astra Rai
+//Version 1 | Array
+//Section Objective: It should have a way to display todos.
+
+//Currently, we're working from the console
+//For now, will print data using console.log
+//You can print items to console.log if you enclose in ()
+//When printing contents of a variable, don't use quotes
+
+var todos = ['item 1', 'item 2', 'item 3'];
+console.log(todos);
+//we want the text "My Todos" to print before list of todos
+console.log('My Todos:', todos);
+//Expected output: "My Todos:" ["item 1", "item 2", "item 3"]
