f9c2ba753a2bf33a520237fa6fa2247aec24aeeb
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-19 01:07:00 -0600

    Version-3-Objects: What's an Object?

---
+++ Version-3-Objects | What's an objects.js
@@ -0,0 +1,16 @@
+//Watch and Code Course
+//Instructor: Gordon Zhu
+//Student: Astra Rai
+//Version 1 | Objects
+//Section Title: What's an object
+
+//Group a set 9f related data that describes a thing
+//Exericse
+var myComputer = {
+  operatingSystem: 'Mac',
+  screenSize: '15 inches',
+  purchaseYear: 2011
+}
+
+console.log(myComputer);
+//expected Output:
