41592e2120d3a4d066671bffd83bc1baf12da468
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-22 15:29:28 -0600

    V3 - Objects | What's an object?

---
+++ Version - 3 - Objects | What's an object.js
@@ -0,0 +1,17 @@
+//Watch and Code Course
+//Instructor
+//Version 1 | Array
+//Section Objective: It should have a place to store todos.
+
+
+//Question: On paper how would you normally write a list.
+//Answer: item 1, item2, item 3
+//Question: In JavaScripts, what's a way to store a list?
+//Answer: In an array, for example: ['item 1', 'item 2', ['item 3']
+//Question: How can we access this array?
+//Array can be accessed by storing array contents in a variable
+//Example of array stored in variable (var): var todos =['item 1', 'item 2' "item 3"]; 
+//Below is an array, named 'todos', with three elements
+var todos = ['item 1', 'item 2', 'item 3']; 
+//print elemetns in array to console.
+console.log(todos);//-->['item 1', 'item 2', 'item 3']
